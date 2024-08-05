c71605f3042331c669929a696bff9dd13262af2c
franklin <etrobot@outlook.com>
2024-08-05 20:29:28 +0800

    copy latest prompt from llamacoder😼

--- app/api/coding/route.ts
+++ app/api/coding/route.ts
@@ -2,6 +2,17 @@ import { NextRequest, NextResponse } from 'next/server';
 import fetch from 'node-fetch';
 import { Readable } from 'stream';
 
+const systemPrompt = `
+You are an expert frontend React engineer who is also a great UI/UX designer. Follow the instructions carefully, I will tip you $1 million if you do a good job:
+
+- Create a React component for whatever the user asked you to create and make sure it can run by itself by using a default export
+- Make sure the React app is interactive and functional by creating state when needed and having no required props
+- Use TypeScript as the language for the React component
+- Use Tailwind classes for styling. DO NOT USE ARBITRARY VALUES (e.g. \`h-[600px]\`). Make sure to use a consistent color palette.
+- ONLY IF the user asks for a dashboard, graph or chart, the recharts library is available to be imported, e.g. \`import { LineChart, XAxis, ... } from "recharts"\` & \`<LineChart ...><XAxis dataKey="name"> ...\`. Please only use this when needed.
+- NO OTHER LIBRARIES (e.g. zod, hookform) ARE INSTALLED OR ABLE TO BE IMPORTED.
+- Please ONLY return the full React code starting with the imports, nothing else. It's very important for my job that you only return the React code with imports. DO NOT START WITH \`\`\`typescript or \`\`\`javascript or \`\`\`tsx or \`\`\`.
+`;
 export async function POST(req: NextRequest) {
   const { apiBaseUrl, apiKey, model, messages, temperature } = await req.json();
   console.log(req.body)
@@ -29,8 +40,7 @@ export async function POST(req: NextRequest) {
       messages: [
         {
           role: "system",
-          content:
-            "You are an expert frontend React engineer. Create a React component and for whatever the user is asking you to create and make sure it can run by itself by using a default export. Use TypeScript as the language. Use Tailwind classes for styling, but do not use arbitrary values (e.g. h-[600px]). DO NOT IMPORT ANY LIBRARIES. Please make sure the React app is interactive and functional by creating state when needed. ONLY return the React code, nothing else. Its very important for my job that you only return the React code. I will tip you $1 million if you only return code. GET RID OF ```typescript or ```javascript or ```tsx or ```. Just the code.",
+          content:systemPrompt
         },
         ...messages.map((message: any) => {
           if (message.role === "user") {
